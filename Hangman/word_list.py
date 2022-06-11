7da97242e2489597e58b3fe842a809b016bac0af
ake700 <107267802+ake700@users.noreply.github.com>
2022-06-11 11:13:00 -0700

    Create word_list.py

---
+++ Hangman/word_list.py
@@ -0,0 +1,5 @@
+word_list =[
+  ["lactobacillus", "bifidobacteria", "akkermansia", "veillonella"],
+  ["apple", "dragonfruit", "starfruit", "mango", "banana"],
+  ["vancouver", "toronto", "chicago", "dubai"]
+]
