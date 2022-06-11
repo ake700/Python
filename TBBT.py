cb3a215611ee75ae086af401b5f63b6cd593196d
ake700 <107267802+ake700@users.noreply.github.com>
2022-06-11 11:34:21 -0700

    Update TBBT.py

--- TBBT.py
+++ TBBT.py
@@ -52,9 +52,11 @@ aa    ]8I 88b,   ,a8" "8a,   ,a8" "8a,   ,aa 88`"Yba,
 '''
 
 import random
+
 game_images = [rock, paper, scissors, lizard, spock]
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors, 3 for lizard, and 4 for Spock.\n"))
+
 if user_choice >= 5 or user_choice < 0:
   print("Invalid number")
 else:
@@ -63,29 +65,21 @@ else:
   computer_choice = random.randint(0, 4)
   print("Computer chose:\n" + game_images[computer_choice])
 
-#check if number is valid FIRST 
+  condition1 = computer_choice == 0 and user_choice == 2
+  condition2 = computer_choice == 0 and user_choice == 3
+  condition3 = computer_choice == 3 and user_choice == 1
+  condition4 = computer_choice == 3 and user_choice == 4
+  condition5 = computer_choice == 2 and user_choice == 3
+  condition6 = computer_choice == 1 and user_choice == 4
+  condition7 = computer_choice == 4 and user_choice == 0
+  condition8 = computer_choice == 4 and user_choice == 2
 
-  if user_choice == 0 and computer_choice == 2:
-    print("Win!")
-  elif computer_choice == 0 and user_choice == 2:
+  conditions = [condition1, condition2, condition3, condition4,
+             condition5, condition6, condition7, condition8]
+    
+  if any(conditions):
     print("Lose")
-  elif computer_choice == 0 and user_choice == 3:
-    print("Lose")
-  elif computer_choice == 3 and user_choice == 4:
-    print("Lose")
-  elif computer_choice == 3 and user_choice == 1:
-    print("Lose")
-   elif computer_choice == 2 and user_choice == 3:
-    print("Lose")
-  elif computer_choice == 1 and user_choice == 4:
-    print("Lose")
-  elif computer_choice == 4 and user_choice == 0:
-    print("Lose")
-  elif computer_choice == 4 and user_choice == 2:
-    print("Lose")
-  elif computer_choice > user_choice: 
-    print("Lose")
-  elif user_choice > computer_choice:
-    print("Win")
   elif user_choice == computer_choice:
     print("Draw")
+  else:
+    print("Win")
