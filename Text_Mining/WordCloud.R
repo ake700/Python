f03428af5dee6594c89a277c719c83f2598e4592
ake700 <107267802+ake700@users.noreply.github.com>
2022-06-14 18:24:04 -0700

    Update WordCloud.R

--- Text_Mining/WordCloud.R
+++ Text_Mining/WordCloud.R
@@ -17,7 +17,8 @@ clean_job <- docs %>%
   tm_map(removePunctuation) %>%
   tm_map(stripWhitespace) %>%
   tm_map(toSpace, "/") %>%
-  tm_map(toSpace, "\\{") %>%
+  #Double backslash \\ is necessary for brackets/parantheses to prevent error                               
+  tm_map(toSpace, "\\{") %>%                                
   tm_map(toSpace, "\\}") %>%
   tm_map(toSpace, ":") %>%
   tm_map(toSpace, ";") %>%
@@ -25,7 +26,8 @@ clean_job <- docs %>%
   tm_map(toSpace, "\u00a0\u00a0\u00a0") %>%
   tm_map(toSpace, "$") %>%
   tm_map(toSpace, "\\W") %>% #all non-words
-  tm_map(removeWords, c("gsk", "role", "you", "the", "into", "will", "job", "use", "large")) %>%
+  #Can customize removeWords                               
+  tm_map(removeWords, c("gsk", "role", "you", "the", "into", "will", "job", "use", "large")) %>% 
   tm_map(stripWhitespace) %>%
   tm_map(content_transformer(tolower)) %>%
   tm_map(removeWords, stopwords("english"))
@@ -38,6 +40,7 @@ d <- data.frame(word = names(v),freq=v)
 head(d, 10) 
 
 set.seed(1234) #for reproducibility
+#Can customize word cloud graphics                               
 wordcloud(words = d$word, freq = d$freq, min.freq = 1, scale = c(2,1),
           max.words=75, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(8, "Dark2"))
